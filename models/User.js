81428834be14f387d3cedb0c61679b53b9ac9c81
wjdwndtlr <wjdwndtlr1024@gmail.com>
2020-05-12 09:41:57 +0900

    BodyParser & PostMan & 회원 가입기능

--- models/User.js
+++ models/User.js
@@ -8,7 +8,7 @@ const userSchema = mongoose.Schema({
   email: {
     type: String,
     trim: true,
-    unique: 1,
+    unique: 1, // 똑같은 이메일은 쓰지못하게끔
   },
   password: {
     type: String,
@@ -19,14 +19,17 @@ const userSchema = mongoose.Schema({
     maxlength: 50,
   },
   role: {
+    // 관리자 설정 Ex) 1은 관리자,0은 일반유저
     type: Number,
     default: 0,
   },
-  image: String,
+  image: String, // 유저 이미지
   token: {
+    // 유효성 검사
     type: String,
   },
   tokenExp: {
+    // token 유효기간 설정
     type: Number,
   },
 });
