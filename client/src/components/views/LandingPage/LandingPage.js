51f98c289e76f3d4e582067e52ac8f4b09a2d1a2
wjdwndtlr <wjdwndtlr1024@gmail.com>
2020-05-13 22:13:16 +0900

    회원 가입 페이지

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,10 +1,6 @@
 import React, { useEffect } from "react";
-import axios from "axios";
 
 function LandingPage() {
-  useEffect(() => {
-    axios.get("/api/hello").then((response) => console.log(response.data));
-  }, []);
   return (
     <div
       style={{
